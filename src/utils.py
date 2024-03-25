5907d766145dad4bcdec358ec93b2268ed570275
Junot974 <antoine.junotpro@gmail.com>
2024-03-25 16:05:45 +0100

    [ADD] Prediction pipeline & add web app

--- src/utils.py
+++ src/utils.py
@@ -49,3 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+
+#Load the pkl file. This is why we need dill
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except CustomException as e:
+        raise CustomException(sys, e)
